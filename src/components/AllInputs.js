48c9a35b25dcf9e8f09499d8be0fb43afa5f767e
Robert Sunde <73792987+robertsunde@users.noreply.github.com>
2021-04-15 17:32:22 -0400

    updating AllInputs component

---
+++ src/components/AllInputs.js
@@ -0,0 +1,6 @@
+import React from "react"
+import API from "../api/api"
+import Searchbar from "./Searchbar"
+import EmployeeTable from "./EmployeeTable"
+
+
---
+++ src/components/EmployeeTable.js
@@ -0,0 +1,45 @@
+import React from "react"
+import Grid from "./Grid"
+
+
+
+
+
+function EmployeeTable({ users, headings, handleFilter }) {
+
+return(
+<div>
+<table>
+<thead>
+          <tr>
+            {headings.map(({ name, width }) => {
+              return (
+                <th
+                  className="col"
+                  key={name}
+                  style={{ width }}
+                  onClick={() => {
+                    handleFilter(name.toLowerCase());
+                  }}
+                >
+                  {name}
+                  <span className="pointer"></span>
+                </th>
+              );
+            })}
+          </tr>
+        </thead>
+
+
+
+
+
+
+
+<Grid users = {users} ></Grid>
+</table>
+</div>
+)
+}
+
+export default EmployeeTable
